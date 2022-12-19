ea9080fe2a6c99c8288df4b8637f160c2bccb8bd
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-12-19 20:53:43 +0200

    update to hooks

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ImageGalleryItem } from 'components/ImageGallery/ImageGalleryItem/ImageGalleryItem';
 
 import { getImeges } from 'services/Api/Api';
@@ -7,78 +7,68 @@ import { Vortex } from 'react-loader-spinner';
 import { Button } from 'components/Button/Button';
 import { toast } from 'react-toastify';
 
-export class ImageGallery extends Component {
-  state = {
-    page: 1,
-    pages: 0,
-    data: [],
-    isLoading: false,
-    error: null,
-  };
+export const ImageGallery = ({ searchQuery, onSelect }) => {
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(0);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(1);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchQuery } = this.props;
-    const { page } = this.state;
+  useEffect(() => {
+    setPage(1);
+    setData([]);
+    setIsLoading(true);
 
-    if (prevProps.searchQuery !== searchQuery) {
-      this.setState({ page: 1, data: [] });
-    }
-    if (prevProps.searchQuery !== searchQuery || prevState.page !== page) {
-      this.setState({ isLoading: true });
+    async function fetchData() {
       try {
         const data = await getImeges(searchQuery, page);
         if (data.totalHits === 0) {
           toast('нема зображень, спробуй ще');
         }
         const totalPages = Math.round(data.total / 12);
-        this.setState({ pages: totalPages });
-        this.setState(prevState => ({
-          data: [...prevState.data, ...data.hits],
-        }));
+        setPages(totalPages);
+        setData([data, ...data.hits]);
       } catch (error) {
-        this.setState({ error: 'от халепа, додаток впав' });
+        setError('от халепа, додаток впав');
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
     }
-  }
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+    fetchData();
+  }, [searchQuery, page]);
+
+  const loadMore = () => {
+    this.setState(page + 1);
   };
 
-  render() {
-    const { error, isLoading, page, pages } = this.state;
-    return (
-      <>
-        <GalleryList className="gallery">
-          {this.state.data.map(({ webformatURL, largeImageURL, id }) => {
-            return (
-              <ImageGalleryItem
-                key={id}
-                smallImg={webformatURL}
-                bigImg={largeImageURL}
-                onSelect={this.props.onSelect}
-              ></ImageGalleryItem>
-            );
-          })}
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-        </GalleryList>
-        {page < pages && <Button onClick={this.loadMore}></Button>}
-        {isLoading && (
-          <Vortex
-            visible={true}
-            height="300"
-            width="300"
-            ariaLabel="vortex-loading"
-            wrapperStyle={{}}
-            wrapperClass="vortex-wrapper"
-            colors={['yellow', 'blue', 'yellow', 'blue', 'blue', 'yellow']}
-          />
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <GalleryList className="gallery">
+        {data.map(({ webformatURL, largeImageURL, id }) => {
+          return (
+            <ImageGalleryItem
+              key={id}
+              smallImg={webformatURL}
+              bigImg={largeImageURL}
+              onSelect={onSelect}
+            ></ImageGalleryItem>
+          );
+        })}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </GalleryList>
+      {page < pages && <Button onClick={loadMore}></Button>}
+      {isLoading && (
+        <Vortex
+          visible={true}
+          height="300"
+          width="300"
+          ariaLabel="vortex-loading"
+          wrapperStyle={{}}
+          wrapperClass="vortex-wrapper"
+          colors={['yellow', 'blue', 'yellow', 'blue', 'blue', 'yellow']}
+        />
+      )}
+    </>
+  );
+};
